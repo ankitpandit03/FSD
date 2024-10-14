4baa74d4f20f701d10cd9c05dabf6b2e0a168721
ankitpandit03 <ankitpanditbihar@gmail.com>
2024-10-14 13:06:56 +0530

    Day6

---
+++ DAY6/script.js
@@ -0,0 +1,40 @@
+// let a=()=>{
+//     console.log("Arrow function");
+// };
+// let b=()=>console.log("Arrow function 2");
+// b();
+// a();
+
+
+
+// let c=(x,y)=>{
+//     return x+y;
+// }
+// let res=c('RAhul','Ankit')
+// let res1=c(12,32);
+// console.log(res,res1);
+
+
+// function display(){
+   // // let p1=document.querySelector("p");
+
+//     let p1=document.querySelectorAll('p');
+//     p1[0].innerHTML="i am in para";
+//     p1[0].style.backgroundColor="red"
+//     p1[1].innerHTML="I am in 2nd PAra";
+//     p1[1].style.backgroundColor="green";
+
+// }
+// function changeheader (){
+//     let head1=document.querySelector("h1");
+//     head1.textContent="Change the title";
+//     let a1=document.querySelector("a");
+//     a1.setAttribute("href","https://aktu.ac.in");
+//     a1.innerHTML="AKTU websites";
+// };
+
+
+let addContent=()=>{
+    const newDiv=document.createElement('div');
+    newDiv.textContent='this is a new div';
+}
