2bf2fd69d40a8c64499b1a668495bf2debd9d8d3
ankitpandit03 <ankitpanditbihar@gmail.com>
2024-11-18 12:58:22 +0530

    dtdfd

---
+++ DAy12/netflix-clone/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import MovieList from "./MovieList"
+
+const App = () => {
+  return (
+    <div>
+      <h1 id="web_name">Netflix Clone App</h1>
+      <MovieList />
+    </div>
+  )
+}
+
+export default App
---
+++ DAy12/netflix-clone/src/movies.js
@@ -0,0 +1,48 @@
+const movies=[
+    {
+        id:1,
+        image:"//rukminim2.flixcart.com/image/612/612/xif0q/movie/4/f/r/2009-dvd-eagle-hindi-tarzan-the-wonder-car-original-imahfdpjpqg85j5d.jpeg?q=70",
+        title:"Tarzan the Wonder",
+        language:"Hindi",
+        cost:250,
+
+
+    },
+    {
+        id:2,
+        image:"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/9/d/d/2013-blu-ray-eagle-hindi-golmaal-original-imagms36uz2hucpy.jpeg?q=70",
+        title:"Glomal",
+        language:"Hindi",
+        cost:300,
+
+
+    },
+    {
+        id:3,
+        image:"https://rukminim2.flixcart.com/image/612/612/av-media/movies/q/w/c/gangs-of-wasseypur-i-original-imadnzhqzxzrgumh.jpeg?q=70",
+        title:"Gangs of wasseypur",
+        language:"Hindi",
+        cost:400,
+
+
+    },
+    {
+        id:4,
+        image:"https://rukminim2.flixcart.com/image/612/612/xif0q/movie/y/x/l/1985-dvd-eagle-hindi-don-amitabh-bachchan-original-imahfkagscjczgy2.jpeg?q=70",
+        title:"Don",
+        language:"Hindi",
+        cost:250,
+
+
+    },
+    {
+        id:5,
+        image:"https://rukminim2.flixcart.com/image/612/612/movie/t/g/d/sultan-2016-hindi-dvd-yashraj-original-imaeht9h4g7skwpz.jpeg?q=70",
+        title:"sultan",
+        language:"Hindi",
+        cost:500,
+
+
+    },
+];
+export default movies; 
